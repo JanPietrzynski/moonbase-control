a158a713300067e95c2328165535737c63b51311
janpietrzynski <jan.pietrzynski@gmail.com>
2026-07-18 15:45:18 +0200

    update

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,6 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Card } from '../../shared/card/card';
 
+export interface StatusCard {
+  title: string;
+  value: number;
+}
+
 @Component({
   selector: 'app-dashboard',
   imports: [Card],
@@ -9,4 +14,24 @@ import { Card } from '../../shared/card/card';
 })
 export class Dashboard {
   protected readonly greeting = signal('Good morning, Commander!');
+
+  totalMissions: StatusCard = {
+    title: 'Total Missions',
+    value: 8,
+  };
+
+  activeMissions: StatusCard = {
+    title: 'Active Missions',
+    value: 2,
+  };
+
+  openIncidents: StatusCard = {
+    title: 'Open Incidents',
+    value: 17,
+  };
+
+  criticalIncidents: StatusCard = {
+    title: 'Critical Incidents',
+    value: 5,
+  };
 }
