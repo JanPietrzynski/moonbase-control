2fad2c38e5aaf3aa4e512e65dd9ac8751e55f621
janpietrzynski <jan.pietrzynski@gmail.com>
2026-07-22 15:00:50 +0200

    update

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,10 +1,5 @@
 import { Component, signal } from '@angular/core';
 
-export interface StatusCard {
-  title: string;
-  value: number;
-}
-
 @Component({
   selector: 'app-dashboard',
   imports: [],
@@ -13,24 +8,4 @@ export interface StatusCard {
 })
 export class Dashboard {
   protected readonly greeting = signal('Good morning, Commander!');
-
-  totalMissions: StatusCard = {
-    title: 'Total Missions',
-    value: 8,
-  };
-
-  activeMissions: StatusCard = {
-    title: 'Active Missions',
-    value: 2,
-  };
-
-  openIncidents: StatusCard = {
-    title: 'Open Incidents',
-    value: 17,
-  };
-
-  criticalIncidents: StatusCard = {
-    title: 'Critical Incidents',
-    value: 5,
-  };
 }
