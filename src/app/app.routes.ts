e0f7340768b80eb339672e237704c556efd81d03
janpietrzynski <jan.pietrzynski@gmail.com>
2026-07-27 10:47:36 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/dashboard';
+import { Missions } from './features/missions/missions';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Dashboard
+  },
+  {
+    path: 'missions',
+    component: Missions
+  }
+];
