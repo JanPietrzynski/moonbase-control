c96adc8ef151b8714c2771892d91796ffd67bf1e
janpietrzynski <jan.pietrzynski@gmail.com>
2026-07-27 14:35:17 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,8 @@ import { Missions } from './features/missions/missions';
 export const routes: Routes = [
   {
     path: '',
-    component: Dashboard
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
   },
   {
     path: 'dashboard',
