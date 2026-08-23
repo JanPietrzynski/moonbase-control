959861df92bdd0970afb49b10829de084f41a1c6
Jan Pietrzyński <jan.pietrzynski@hicron.com>
2026-08-23 12:50:45 +0200

    update

--- src/app/features/incidents/report-incident/report-incident.ts
+++ src/app/features/incidents/report-incident/report-incident.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IncidentSeverity } from '../incidents.service';
+import { form, FormField } from '@angular/forms/signals';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+interface IncidentFormData {
+  title: string;
+  description: string;
+  severity: IncidentSeverity | '';
+}
 
 @Component({
   selector: 'app-report-incident',
-  imports: [],
+  imports: [
+    FormField,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule],
   templateUrl: './report-incident.html',
   styleUrl: './report-incident.scss',
 })
-export class ReportIncident {}
+export class ReportIncident {
+  readonly severities = Object.values(IncidentSeverity);
+  readonly incidentModel = signal<IncidentFormData>({
+    title: '',
+    description: '',
+    severity: '',
+  });
+
+  readonly incidentForm = form(this.incidentModel);
+}
