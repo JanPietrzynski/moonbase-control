1ff5264343856c6f76827ff486d89e52ade17f00
janpietrzynski <jan.pietrzynski@gmail.com>
2026-08-30 10:25:54 +0200

    update

--- src/app/features/incidents/report-incident/report-incident.ts
+++ src/app/features/incidents/report-incident/report-incident.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { IncidentSeverity } from '../incidents.service';
-import { form, FormField } from '@angular/forms/signals';
+import { form, FormField, minLength, required } from '@angular/forms/signals';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -29,5 +29,18 @@ export class ReportIncident {
     severity: '',
   });
 
-  readonly incidentForm = form(this.incidentModel);
+  readonly incidentForm = form(this.incidentModel, (schemaPath) =>  {
+    required(schemaPath.title, {
+      message: 'Title is required',
+    });
+
+    minLength(schemaPath.title, 5, {
+      message: 'Title must contain at least 5 characters',
+    });
+
+    required(schemaPath.severity, {
+      message: 'Severity is required',
+    });
+
+  });
 }
